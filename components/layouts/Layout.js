bea35e3e0c31d7012be6ed444380f5f5b464b75f
StR0vErBG <xcreaw1@abv.bg>
2023-03-09 20:15:35 +0200

    adding components for the layout

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./Footer";
+import Navigation from "./Navigation";
 
-const Layout = () => {
-  return <div></div>;
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navigation />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
