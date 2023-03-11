d0ddcc62639a9ffed80118da57d9e096d744eb65
StR0vErBG <xcreaw1@abv.bg>
2023-03-11 21:13:07 +0200

    no time for proper comment

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="bg-[#f7fcfd] min-h-screen flex justify-between flex-col">
       <Navigation />
       {children}
       <Footer />
