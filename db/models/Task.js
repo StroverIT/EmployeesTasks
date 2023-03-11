d29522da643beb1d456986670b8158e40a8b7c3b
StR0vErBG <xcreaw1@abv.bg>
2023-03-11 21:32:25 +0200

    Task model

---
+++ db/models/Task.js
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+
+const TaskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  assignee: {
+    type: String,
+    required: true,
+  },
+  assigneeId: { type: Schema.Types.ObjectId, ref: "Employee" },
+  description: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: String,
+    required: true,
+  },
+  isCompleted: Bolean,
+
+  createdAt: {
+    type: String,
+    default: () => {
+      return new Date(Date.now()).toLocaleDateString();
+    },
+  },
+});
+
+const Task = models.Task || model("Task", TaskSchema);
+
+export default Task;
