7d207aca17dd6ccd111064b93320508a369d515b
StR0vErBG <xcreaw1@abv.bg>
2023-03-12 17:53:29 +0200

    Employee component with view and edit

--- components/indexComp/Employee.js
+++ components/indexComp/Employee.js
@@ -1,19 +1,53 @@
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { formatPercentage, generateRandomColor } from "../../utils/helper";
+import { formatCurrency } from "../../utils/helper";
+import { getCompletedAndNon } from "../../utils/task";
 import Button from "../Forms/Buttons/Default";
 
-const Employee = ({ color }) => {
+const Employee = ({ data, type, isFullData }) => {
+  const router = useRouter();
+
+  const [tasks, setTasks] = useState({
+    completed: 0,
+    total: 0,
+  });
+
+  useEffect(() => {
+    (async function () {
+      const res = await getCompletedAndNon(data._id);
+      setTasks(res.data);
+    })();
+  }, [data]);
+
+  let range, onClick, text;
+
+  if (tasks.completed == 0) {
+    range = 0;
+  } else {
+    range = (tasks.completed / tasks.total) * 100;
+  }
+
+  if (type == "view") {
+    text = "View Details";
+    onClick = function () {
+      return router.push(`/account/${data._id.toString()}`);
+    };
+  } else if (type == "edit") {
+    text = "Edit Account";
+  }
   return (
     <section
       className={`box-shadow border-[3px]  rounded-[1.3rem] px-10 py-6  w-full`}
       style={{
-        borderColor: color,
-        color,
+        borderColor: data.color,
+        color: data.color,
       }}
     >
       <section className={` flex justify-between items-center w-full `}>
-        <div className="font-bold text-2xl">Emil Zlatinov</div>
-        <div className="text-xl font-medium">10 / 50 Tasks</div>
+        <div className="font-bold text-2xl">{data.fullName}</div>
+        <div className="text-xl font-medium">
+          {tasks.completed} / {tasks.total} Tasks
+        </div>
       </section>
 
       {/* Range */}
@@ -21,20 +55,42 @@ const Employee = ({ color }) => {
         <div
           className=" h-4 rounded-full dark:bg-red-500"
           style={{
-            width: "15" + "%",
-            backgroundColor: color,
+            width: range + "%",
+            backgroundColor: data.color,
           }}
         ></div>
       </div>
-
-      <section className="flex justify-between items-center w-full text-sm">
-        <div>
-          Monthly salary:<span className="pl-1 text-slate-500"> $1200.50</span>
-        </div>
+      <section className="flex flex-col gap-y-2">
+        <section className="flex justify-between items-center w-full text-sm">
+          <div>Email</div>
+          <div className="pl-1 text-slate-500"> {data.email}</div>
+        </section>
+        {isFullData && (
+          <>
+            <section className="flex justify-between items-center w-full text-sm">
+              <div>Phone Number</div>
+              <div className="pl-1 text-slate-500"> {data.phoneNumber}</div>
+            </section>
+            <section className="flex justify-between items-center w-full text-sm">
+              <div>Birthday</div>
+              <div className="pl-1 text-slate-500"> {data.birthday}</div>
+            </section>
+            <section className="flex justify-between items-center w-full text-sm">
+              <div>Month Salary</div>
+              <div className="pl-1 text-slate-500">
+                {formatCurrency(data.monthSalary)}
+              </div>
+            </section>
+          </>
+        )}
       </section>
-
       <div className="flex-center w-full mt-7">
-        <Button text="View Details" className={`w-44 `} color={color} />
+        <Button
+          text={text}
+          className={`w-44 `}
+          color={data.color}
+          onClick={onClick}
+        />
       </div>
     </section>
   );
