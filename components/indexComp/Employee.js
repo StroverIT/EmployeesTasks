e9529afd79c5c525e517d6cf5ded3af00373cf7a
StR0vErBG <xcreaw1@abv.bg>
2023-03-12 22:00:04 +0200

    adding more functionalities

--- components/indexComp/Employee.js
+++ components/indexComp/Employee.js
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { createEmployeeInputs } from "../../data/index/Inputs";
 import { toastError, toastSuccess } from "../../libs/Notifications";
-import { del } from "../../utils/employee";
-import { formatCurrency } from "../../utils/helper";
+import { del, edit } from "../../utils/employee";
+import {
+  formatCurrency,
+  inputsToState,
+  setInputHandler,
+} from "../../utils/helper";
 import { getCompletedAndNon } from "../../utils/task";
 import Button from "../Forms/Buttons/Default";
+import Input from "../Forms/Inputs/Default";
 
 const Employee = ({ data, type, isFullData }) => {
   const router = useRouter();
@@ -12,8 +18,16 @@ const Employee = ({ data, type, isFullData }) => {
     completed: 0,
     total: 0,
   });
+  const [isEdit, setEdit] = useState(false);
+  const [employeeInputs, setEmployeeInputs] = useState(data);
+  const [isLoading, setLoading] = useState(false);
+  const [editLoading, setEditLoading] = useState(false);
 
+  const inputHandler = (e) => setInputHandler(e, setEmployeeInputs);
+
+  // ------ DELETE -------
   const deleteHandler = async () => {
+    setLoading(true);
     const res = await del(data._id);
     if (res.message) {
       toastSuccess(res.message);
@@ -21,7 +35,23 @@ const Employee = ({ data, type, isFullData }) => {
     if (res.error) {
       toastError(res.error);
     }
+    setLoading(false);
     router.push("/", undefined, { scroll: false });
+  };
+
+  // ------ DELETE -------
+  const editHandler = async () => {
+    setEditLoading(true);
+    const res = await edit(employeeInputs, data._id);
+    if (res.message) {
+      toastSuccess(res.message);
+      setEdit(false);
+      router.push(router.asPath, undefined, { scroll: false });
+    }
+    if (res.error) {
+      toastError(res.error);
+    }
+    setEditLoading(false);
   };
 
   let range, onClick, text;
@@ -38,7 +68,11 @@ const Employee = ({ data, type, isFullData }) => {
     };
   } else if (type == "edit") {
     text = "Edit Account";
+    onClick = function () {
+      setEdit(true);
+    };
   }
+
   useEffect(() => {
     (async function () {
       const res = await getCompletedAndNon(data._id);
@@ -54,7 +88,17 @@ const Employee = ({ data, type, isFullData }) => {
       }}
     >
       <section className={` flex justify-between items-center w-full `}>
-        <div className="font-bold text-2xl">{data.fullName}</div>
+        {!isEdit && <div className="font-bold text-2xl">{data.fullName}</div>}
+        <div>
+          {isEdit && (
+            <Input
+              {...createEmployeeInputs[0]}
+              onChange={inputHandler}
+              value={employeeInputs[createEmployeeInputs[0].name]}
+              isLabel={false}
+            />
+          )}
+        </div>
         <div className="text-xl font-medium">
           {tasks.completed} / {tasks.total} Tasks
         </div>
@@ -73,35 +117,86 @@ const Employee = ({ data, type, isFullData }) => {
       <section className="flex flex-col gap-y-2">
         <section className="flex justify-between items-center w-full text-sm">
           <div>Email</div>
-          <div className="pl-1 text-slate-500"> {data.email}</div>
+          {!isEdit && <div className="pl-1 text-slate-500"> {data.email}</div>}
+          {isEdit && (
+            <Input
+              {...createEmployeeInputs[1]}
+              onChange={inputHandler}
+              value={employeeInputs[createEmployeeInputs[1].name]}
+              isLabel={false}
+            />
+          )}
         </section>
         {isFullData && (
           <>
             <section className="flex justify-between items-center w-full text-sm">
               <div>Phone Number</div>
-              <div className="pl-1 text-slate-500"> {data.phoneNumber}</div>
+              {!isEdit && (
+                <div className="pl-1 text-slate-500"> {data.phoneNumber}</div>
+              )}
+              {isEdit && (
+                <Input
+                  {...createEmployeeInputs[2]}
+                  onChange={inputHandler}
+                  value={employeeInputs[createEmployeeInputs[2].name]}
+                  isLabel={false}
+                />
+              )}
             </section>
             <section className="flex justify-between items-center w-full text-sm">
               <div>Birthday</div>
-              <div className="pl-1 text-slate-500"> {data.birthday}</div>
+              {!isEdit && (
+                <div className="pl-1 text-slate-500"> {data.birthday}</div>
+              )}
+              {isEdit && (
+                <Input
+                  {...createEmployeeInputs[3]}
+                  onChange={inputHandler}
+                  value={employeeInputs[createEmployeeInputs[3].name]}
+                  isLabel={false}
+                />
+              )}
             </section>
             <section className="flex justify-between items-center w-full text-sm">
               <div>Month Salary</div>
-              <div className="pl-1 text-slate-500">
-                {formatCurrency(data.monthSalary)}
-              </div>
+              {!isEdit && (
+                <div className="pl-1 text-slate-500">
+                  {formatCurrency(data.monthSalary)}
+                </div>
+              )}
+              {isEdit && (
+                <Input
+                  {...createEmployeeInputs[4]}
+                  onChange={inputHandler}
+                  value={employeeInputs[createEmployeeInputs[4].name]}
+                  isLabel={false}
+                />
+              )}
             </section>
           </>
         )}
       </section>
-      <div className="flex-center w-full mt-7">
-        <Button
-          text={text}
-          className={`w-44 `}
-          color={data.color}
-          onClick={onClick}
-        />
-      </div>
+      {!isEdit && (
+        <div className="flex-center w-full mt-7">
+          <Button
+            text={text}
+            className={`w-44 `}
+            color={data.color}
+            onClick={onClick}
+          />
+        </div>
+      )}
+      {isEdit && (
+        <div className="flex-center w-full mt-7">
+          <Button
+            text="Send"
+            className={`w-44 `}
+            color={data.color}
+            onClick={editHandler}
+            isLoading={editLoading}
+          />
+        </div>
+      )}
       <div className="absolute -top-2 -right-2">
         <Button
           icon="trash"
@@ -109,6 +204,7 @@ const Employee = ({ data, type, isFullData }) => {
           className={`w-12 `}
           color={"#df6b62"}
           onClick={deleteHandler}
+          isLoading={isLoading}
           classNameIcon="text-xl"
         />
       </div>
