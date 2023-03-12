5889c5757324d61d39a8d31383ec0511910264bb
StR0vErBG <xcreaw1@abv.bg>
2023-03-12 17:54:53 +0200

    Fully dynamic monthly component

--- components/indexComp/TopFiveForMonth.js
+++ components/indexComp/TopFiveForMonth.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// Components
+import TopFiveEmployee from "./TopFiveEmployee";
+
+// API
+import { topFiveTasksMonthly } from "../../utils/task";
 
 const TopFiveForMonth = () => {
+  const [employeesAndTasks, setEmployeesAndTasks] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const res = await topFiveTasksMonthly();
+      setEmployeesAndTasks(res.data);
+    })();
+  }, []);
   return (
     <section>
       <h3 className="mb-10 text-4xl font-semibold">
@@ -8,23 +20,24 @@ const TopFiveForMonth = () => {
       </h3>
 
       <section className="box-shadow border-[3px] border-blue-300 rounded-[1.3rem] px-10 py-6 grid grid-cols-2 justify-between items-center">
-        <section className="mb-5">
-          <div className="inline-block text-xl font-medium">
+        <section className="mb-5 text-center">
+          <div className="inline-block text-xl font-medium ">
             Employee Name
             <div className="w-full h-[2px] bg-blue-600"></div>
           </div>
         </section>
-        <section className="mb-5">
+        <section className="mb-5 text-center">
           <div className="inline-block text-xl font-medium">
-            Total Completed Tasks
+            Completed Tasks
             <div className="w-full h-[2px] bg-blue-600 "></div>
           </div>
         </section>
 
         {/* This must be filtered from most completed tasks emoployee */}
 
-        <section className="text-lg ">Ivan Kirkov</section>
-        <section className="text-lg font-semibold text-green-500">100</section>
+        {employeesAndTasks.map((data) => {
+          return <TopFiveEmployee key={data._id} data={data} />;
+        })}
       </section>
     </section>
   );
