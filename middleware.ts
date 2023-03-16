c0d0481b97753208444bacc501de601e1a1474a2
StR0vErBG <xcreaw1@abv.bg>
2023-03-16 20:22:24 +0200

    typo mistake

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,6 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   return NextResponse.next();
-  a;
 }
 
 // See "Matching Paths" below to learn more
