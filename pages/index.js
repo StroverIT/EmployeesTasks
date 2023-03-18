ced2cd379fa63f2152de723ddad73eb5ed7f2f9e
StR0vErBG <xcreaw1@abv.bg>
2023-03-18 08:43:47 +0200

    returning create tasks

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,8 @@ export default function Home({ totalEmployeeLength, namesAndIds, employees }) {
         {/* Creating a employee */}
         <section className="grid grid-cols-1 gap-10 mt-16 md:grid-cols-2">
           <CreateEmployee />
-          {/* {totalEmployeeLength > 0 && <CreateTask namesAndIds={namesAndIds} />}
-          {totalEmployeeLength > 0 && (
+          {totalEmployeeLength > 0 && <CreateTask namesAndIds={namesAndIds} />}
+          {/* {totalEmployeeLength > 0 && (
             <TopFiveForMonth namesAndIds={namesAndIds} />
           )} */}
           {/* {totalEmployeeLength > 0 && <StatisticForTheYear />} */}
